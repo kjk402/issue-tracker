0517c94fe8bdfdf9f696ccb626b7c7a4d40803cc
JoonKi-Kim <joonki402@gmail.com>
2021-06-08 15:57:18 +0900

    fix: move config directory

--- backend/src/main/java/com/bas/issuetracker/web/config/SwaggerConfig.java
+++ backend/src/main/java/com/bas/issuetracker/web/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.bas.issuetracker.config;
+package com.bas.issuetracker.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- backend/src/main/java/com/bas/issuetracker/web/config/WebConfig.java
+++ backend/src/main/java/com/bas/issuetracker/web/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.bas.issuetracker.config;
+package com.bas.issuetracker.web.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
