836ccd65279ef56c15b024fd9b44d272d16cb12e
1song2 <1song2918@gmail.com>
2021-06-09 15:02:24 +0900

    feat: add login buttons to login page (#5)

--- iOS/IssueTrackerApp/IssueTrackerApp/LoginViewController.swift
+++ iOS/IssueTrackerApp/IssueTrackerApp/LoginViewController.swift
@@ -8,9 +8,24 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-    @IBOutlet weak var loginProviderStackView: UIStackView!
+    @IBOutlet weak var gitHubLoginButton: UIButton!
+    @IBOutlet weak var appleLoginButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        configureGitHubLoginButton()
+        configureAppleLoginButton()
+    }
+
+    private func configureGitHubLoginButton() {
+        gitHubLoginButton.layer.masksToBounds = false
+        gitHubLoginButton.layer.cornerRadius = 20.0
+        gitHubLoginButton.imageView?.contentMode = .scaleAspectFit
+    }
+
+    private func configureAppleLoginButton() {
+        appleLoginButton.layer.masksToBounds = false
+        appleLoginButton.layer.cornerRadius = 20.0
     }
 }
