88c8d641b572c262cff2aef6cd61f87289f4dd94
1song2 <1song2918@gmail.com>
2021-06-09 12:28:38 +0900

    feat: update login page layout (#5)

--- iOS/IssueTrackerApp/IssueTrackerApp/LoginViewController.swift
+++ iOS/IssueTrackerApp/IssueTrackerApp/LoginViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    @IBOutlet weak var loginProviderStackView: UIStackView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
