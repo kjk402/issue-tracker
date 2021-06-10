9a82b5c7b9ce3854c6a0e5b2e354a7a8f1a2088e
Malloc72P <41663269+Malloc72P@users.noreply.github.com>
2021-06-10 17:49:42 +0900

    feat: 레이블과 마일스톤에 대한 스웨거 추가

--- backend/src/main/java/com/bas/issuetracker/web/controller/MilestoneController.java
+++ backend/src/main/java/com/bas/issuetracker/web/controller/MilestoneController.java
@@ -7,10 +7,13 @@ import com.bas.issuetracker.web.dto.milestone.MilestonePreviews;
 import com.bas.issuetracker.web.dto.milestone.MilestoneState;
 import com.bas.issuetracker.web.service.milestone.MilestoneDtoConverter;
 import com.bas.issuetracker.web.service.milestone.MilestoneService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@Api(tags = {"Milestone Controller"})
 @RestController
 @RequestMapping("/milestones")
 public class MilestoneController {
@@ -24,23 +27,27 @@ public class MilestoneController {
     }
 
     @PostMapping
+    @ApiOperation(value = "마일스톤 생성", notes = "새로운 마일스톤을 생성합니다.")
     public MilestonePreview createMilestone(@RequestBody @Valid MilestoneMetadata metadata) {
         Milestone milestone = milestoneService.createMilestone(metadata);
         return milestoneDtoConverter.milestoneToMilestonePreview(milestone);
     }
 
     @GetMapping
+    @ApiOperation(value = "마일스톤 목록조회", notes = "마일스톤을 목록을 조회합니다.")
     public MilestonePreviews showMilestones() {
         return milestoneService.showMilestones();
     }
 
     @PatchMapping("/{milestoneId}")
+    @ApiOperation(value = "마일스톤 수정", notes = "마일스톤을 수정합니다.")
     public void updateMilestone(@PathVariable int milestoneId,
                                 @RequestBody @Valid MilestoneMetadata metadata) {
         milestoneService.updateMilestone(milestoneId, metadata);
     }
 
     @PatchMapping("/{milestoneId}/state")
+    @ApiOperation(value = "마일스톤 상태변경", notes = "마일스톤을 열거나 닫습니다")
     public void updateMilestoneState(@PathVariable int milestoneId,
                                      @RequestBody @Valid MilestoneState state) {
         if (state.isOpen()) {
@@ -51,6 +58,7 @@ public class MilestoneController {
     }
 
     @DeleteMapping("/{milestoneId}")
+    @ApiOperation(value = "마일스톤 삭제", notes = "마일스톤을 삭제합니다")
     public void deleteMilestone(@PathVariable int milestoneId) {
         milestoneService.deleteMilestone(milestoneId);
     }
