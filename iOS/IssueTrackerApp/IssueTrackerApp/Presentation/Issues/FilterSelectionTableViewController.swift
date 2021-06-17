1763006bc4a2a501ede4278cf4dea711e4bfb011
1song2 <1song2918@gmail.com>
2021-06-17 17:58:23 +0900

    feat: add view models for filter selection (#36)

--- iOS/IssueTrackerApp/IssueTrackerApp/Presentation/Issues/FilterSelectionTableViewController.swift
+++ iOS/IssueTrackerApp/IssueTrackerApp/Presentation/Issues/FilterSelectionTableViewController.swift
@@ -8,13 +8,49 @@
 import UIKit
 
 class FilterSelectionTableViewController: UITableViewController {
+    private var viewModel: FilterSelectionViewModel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "필터"
+        viewModel = makeFilterSelectionViewModel()
 
+        navigationItem.title = viewModel.screenTitle
         configureCancelBarButtonItem()
         configureSaveBarButtonItem()
+    }
+
+    private func makeFilterSelectionViewModel() -> FilterSelectionViewModel {
+        return FilterSelectionViewModel(sections: [
+            makeSectionViewModel(headerTitle: "상태", filters: [
+                makeFilterViewModel(name: "열린 이슈", isSelected: true),
+                makeFilterViewModel(name: "내가 작성한 이슈", isSelected: false),
+                makeFilterViewModel(name: "나에게 할당된 이슈", isSelected: false),
+                makeFilterViewModel(name: "내가 댓글을 남긴 이슈", isSelected: false),
+                makeFilterViewModel(name: "닫힌 이슈", isSelected: false)
+            ]),
+            makeSectionViewModel(headerTitle: "작성자", filters: [
+                makeFilterViewModel(name: "JK", isSelected: false),
+                makeFilterViewModel(name: "Honux", isSelected: false)
+            ]),
+            makeSectionViewModel(headerTitle: "레이블", filters: [
+                makeFilterViewModel(name: "레이블 없음", isSelected: false),
+                makeFilterViewModel(name: "Documentation", isSelected: false),
+                makeFilterViewModel(name: "bug", isSelected: false)
+            ]),
+            makeSectionViewModel(headerTitle: "마일스톤", filters: [
+                makeFilterViewModel(name: "마일스톤 없음", isSelected: false),
+                makeFilterViewModel(name: "마스터즈 코스", isSelected: false)
+            ])
+        ])
+    }
+
+    private func makeSectionViewModel(headerTitle: String,
+                                      filters: [FilterViewModel]) -> SectionViewModel {
+        return SectionViewModel(headerTitle: headerTitle, filters: filters)
+    }
+
+    private func makeFilterViewModel(name: String, isSelected: Bool) -> FilterViewModel {
+        return FilterViewModel(name: name, isSelected: isSelected)
     }
 
     private func configureCancelBarButtonItem() {
