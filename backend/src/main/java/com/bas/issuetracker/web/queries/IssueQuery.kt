4286a32d2cedbd5bc2a5a3708aec1468ae8d705f
JoonKi-Kim <joonki402@gmail.com>
2021-06-18 11:23:19 +0900

    feat: query문 이동, 코멘트 시간 업데이트 수정

--- backend/src/main/java/com/bas/issuetracker/web/queries/IssueQuery.kt
+++ backend/src/main/java/com/bas/issuetracker/web/queries/IssueQuery.kt
@@ -1,13 +1,5 @@
 package com.bas.issuetracker.web.queries;
 
-const val SELECT_MULTIPLE_ISSUE: String = """
-SELECT DISTINCT i.id, i.title, i.is_open, (((SELECT COUNT(*) FROM comment WHERE issue_id = i.id) -1)) AS comment_count, i.last_modified_date_time, u.id AS user_id, u.nickname, u.name, u.profile_image
-FROM issue AS i
-inner JOIN comment c ON c.issue_id = i.id
-inner JOIN user u ON u.id = i.author_id
-WHERE i.is_open = :open_or_close AND i.author_id = u.id AND i.id = c.issue_id;
-"""
-
 const val SELECT_ISSUE_DETAIL: String = """
 SELECT DISTINCT i.id, i.title, i.is_open, (((SELECT COUNT(*) FROM comment WHERE issue_id = i.id) -1)) AS comment_count, i.last_modified_date_time, u.id AS user_id,u.nickname, u.name, u.profile_image
 FROM issue AS i
@@ -33,3 +25,39 @@ and i.author_id = :issue_author_id"""
 
 const val FILTER_PART_END_OF_QUERY: String = """
 group by i.id;"""
+
+const val CREATE_ISSUE: String = """
+INSERT INTO issue (title, author_id, is_open, milestone_id,last_modified_date_time)
+VALUES (:title, :author_id, :is_open, :milestone_id, :last_modified_date_time);
+"""
+
+const val UPDATE_ISSUE: String = """
+UPDATE issue SET title = :title , last_modified_date_time = now() WHERE id = :issue_id;    
+"""
+
+const val OPEN_OR_CLOSE_ISSUE: String = """
+UPDATE issue SET is_open = :state WHERE id IN (:issue_ids);    
+"""
+
+const val DISCONNECT_LABEL: String = """
+DELETE FROM issue_label WHERE issue_id = :issue_id;    
+"""
+
+const val CONNECT_LABEL: String = """
+INSERT INTO issue_label (issue_id, label_id)
+VALUES (:issue_id, :label_id);    
+"""
+
+const val UPDATE_MILESTONE_ISSUE: String = """
+UPDATE issue SET milestone_id = :milestone_id WHERE id = :issue_id;    
+"""
+
+const val DISCONNECT_ASSIGNED: String = """
+DELETE FROM assigned WHERE issue_id = :issue_id;  
+"""
+
+const val CONNECT_ASSIGNED: String = """
+INSERT INTO assigned (issue_id, user_id)
+VALUES (:issue_id, :user_id);    
+"""
+
