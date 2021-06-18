a4f94d67b8ba11767e230519180495ff458cd044
1song2 <1song2918@gmail.com>
2021-06-18 17:35:18 +0900

    feat: extend UIFont to set preferred font weight (#50)

---
+++ iOS/IssueTrackerApp/IssueTrackerApp/UIFont+PreferredFontWeight.swift
@@ -0,0 +1,17 @@
+//
+//  UIFont+PreferredFontWeight.swift
+//  IssueTrackerApp
+//
+//  Created by Song on 2021/06/18.
+//
+
+import UIKit
+
+extension UIFont {
+    static func preferredFont(for style: TextStyle, weight: Weight) -> UIFont {
+        let metrics = UIFontMetrics(forTextStyle: style)
+        let desc = UIFontDescriptor.preferredFontDescriptor(withTextStyle: style)
+        let font = UIFont.systemFont(ofSize: desc.pointSize, weight: weight)
+        return metrics.scaledFont(for: font)
+    }
+}
